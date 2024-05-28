cd549a7dfa9e438e702ee720680bb51d43d2151b
Anthony Briceño Quiroz <anthonyquiroz305@gmail.com>
2024-05-27 20:33:12 -0500

    Tarea - Practica Grupal 25/05/2024

---
+++ Tarea - Practica Grupal 25_05_20224/Ejercicio_poisson1.R
@@ -0,0 +1,49 @@
+#Tarea Grupal - EJERCICIO DE POISSON
+
+# Un vendedor de seguros de vida vende un promedio de 4 polizas
+# por semana, Calcular la probabilidad de:
+
+# 1) Que venda alguna pólizas en una semana
+# 2) Que venda 2 o más pólizas, pero menos de 5 (en una semana)
+# 3) Suponiendo que hay 5 días de trabajo por semana, ¿cuál es la probabilidad de que en un día dado venda una póliza?
+# 4) Calcule la media, la varianza y la desviación estándar de la distribución de probabilidad que se infiere de este problema
+
+library(dplyr)
+
+# Parámetro de la distribución de Poisson
+# X = Número de polizas vendidas en 7 dias(1 semana)
+lambda <- 4
+
+#  Que venda algunas pólizas en una semana
+p_al_menos_una <- 1 - dpois(0, 4)
+p <- round(p_al_menos_una,4)
+cat("Probabilidad de que venda alguna pólizas en una semana ", p_al_menos_una, "\n")
+cat("Probabilidad de que venda alguna pólizas en una semana (4 decimales):", p, "\n")
+
+# Probabilidad de vender 2, 3, o 4 pólizas en una semana
+p_2 <- dpois(2,lambda)
+p_3 <- dpois(3,lambda)
+p_4 <- dpois(4,lambda)
+p_2_5 <- p_2 + p_3 + p_4
+p <- round(p_2_5,4)
+
+cat("Probabilidad de vender entre 2 y 4 pólizas en una semana:", p_2_5,"\n")
+cat("Probabilidad de vender entre 2 y 4 pólizas en una semana (4 decimales):", p,"\n")
+
+# Suponiendo que hay 5 días de trabajo por semana, ¿cuál es la probabilidad de que en un día dado venda una póliza?
+lambda_dia <- lambda / 5
+p_una_poliza_dia <- dpois(1, lambda_dia)
+p <- round(p_una_poliza_dia,4)
+
+cat("Probabilidad de vender una póliza en un día dado:", p_una_poliza_dia,"\n")
+cat("Probabilidad de vender una póliza en un día dado (4 decimales):", p,"\n")
+
+# Calcule la media, la varianza y la desviación estándar de la distribución de probabilidad que se infiere de este problema
+media <- lambda
+varianza <- lambda
+desviacion_estandar <- sqrt(lambda)
+
+cat("Media:", media, "\n")
+cat("Varianza:", varianza, "\n")
+cat("Desviación estándar:", desviacion_estandar, "\n")
+
